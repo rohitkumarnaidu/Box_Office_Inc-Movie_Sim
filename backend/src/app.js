99b0c7da5f11171232cd36972228a16c8f066d8e
Amisha Pandey <amisha.pandey2006@gmail.com>
2026-07-09 15:33:06 +0530

    Add express-rate-limit middleware import

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,8 +3,8 @@ import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
 import cookieParser from "cookie-parser";
+import rateLimit from "express-rate-limit";
 import env from "./config/env.js";
-
 const app = express();
 
 /*
